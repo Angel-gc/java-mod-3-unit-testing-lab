03fd9c5d9eaac9d1b467c76c18f82a86593f4742
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-17 12:00:38 -0400

    refactored

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,13 +1,12 @@
-import java.util.Locale;
 
 public class FizzBuzz {
-  public String fizzBuzzString(String str) {
-      if (str == null) return null;
-      if (str.toLowerCase().startsWith("f") && str.toLowerCase().endsWith("b")) return "FizzBuzz";
-      if (str.toLowerCase().startsWith("f"))  return "Fizz";
-      if (str.toLowerCase().endsWith("b")) return "Buzz";
+    public String fizzBuzzString(String str) {
+        if(str == null) return null;
+        String normalizedString = str.toLowerCase();
+        if (normalizedString.startsWith("f") && normalizedString.endsWith("b")) return "FizzBuzz";
+        if (normalizedString.startsWith("f"))  return "Fizz";
+        if (normalizedString.endsWith("b")) return "Buzz";
 
-
-      return str;
-  }
+        return str;
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -12,14 +12,14 @@ class MainTest {
     void nullTest(){
         FizzBuzz fb = new FizzBuzz();
         String nullStr = fb.fizzBuzzString(null);
-
         assertNull(nullStr);
     }
     @Test
     void startsWithF(){
         FizzBuzz fb = new FizzBuzz();
         String fStr = fb.fizzBuzzString("fire");
-
+        String fs = fb.fizzBuzzString("FIRE");
+        assertEquals("Fizz", fs);
         assertEquals("Fizz", fStr);
     }
 
@@ -35,7 +35,15 @@ class MainTest {
     void startsWithFEndsWithB(){
         FizzBuzz fb = new FizzBuzz();
         String fbStr = fb.fizzBuzzString("firb");
-
+        String fbSt = fb.fizzBuzzString("Fb");
         assertEquals("FizzBuzz", fbStr);
+        assertEquals("FizzBuzz", fbSt);
+
+    }
+    @Test
+    void returnsString() {
+        FizzBuzz fb = new FizzBuzz();
+        String fbStr = fb.fizzBuzzString("other");
+        assertEquals("other", fbStr);
     }
 }
